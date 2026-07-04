78eaef0e6dc48e3aa966c9d546012200b23b6e67
Chandana <chandana66266@gmail.com>
2026-07-04 22:37:25 +0530

    css learning

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  '../styles/Button.css'
+
+const Button = () => {
+  return (
+    <div className='button'>
+      this is button
+    </div>
+  )
+}
+
+export default Button
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Header.css';
+
+const Header = () => {
+  return (
+    <div className='head'>
+      <h1>header</h1>
+      <button>login</button>
+    </div>
+  )
+}
+
+export default Header
