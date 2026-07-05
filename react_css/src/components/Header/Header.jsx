d8fc95df92e19823909ba7751909119d67dbc741
Chandana <chandana66266@gmail.com>
2026-07-05 12:44:46 +0530

    git commit message

---
+++ react_css/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Header.css'
+const Header = () => {
+  return (
+    <div className='head'>
+      <h3>youtube channel</h3>
+      <button>LOGIN</button>
+    </div>
+  )
+}
+
+export default Header
---
+++ react_css/src/components/button/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Header.module.css'
+const Button = () => {
+  return (
+    <div >
+      BUTTON
+    </div>
+  )
+}
+
+export default Button
