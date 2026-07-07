2c13716c1278b4a353b8dfa037ef6727683e4ff9
Chandana <chandana66266@gmail.com>
2026-07-07 17:14:23 +0530

    done by first commit

---
+++ UI_PROJECT/src/components/section1/Page1content.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Leftcontent from './Leftcontent'
+import Rightcontent from './Rightcontent'
+const Page1content = () => {
+  return (
+    <div className='py-4 h-[90vh] bg-amber-800 px-16 ' > 
+
+    <Leftcontent />
+    <Rightcontent />
+    </div>
+  )
+}
+
+export default Page1content
