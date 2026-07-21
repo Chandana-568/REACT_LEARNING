6537f054cf9cbcf0eaef65b0ddf0fdc9f9772448
Chandana <chandana66266@gmail.com>
2026-07-21 20:13:26 +0530

    done with first commit

---
+++ api-calling/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import axios from 'axios'
+
+const App = () => {
+const getData= ()=>{
+ 
+  
+}
+  return (
+    <div>
+     <button onClick={getData}>Get Data </button> 
+    </div>
+  )
+}
+
+export default App
